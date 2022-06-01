022be6d546d8154f66ce6b255b800f2d6c9baff3
Vladyslav Avidzba <wasssd1@gmail.com>
2022-06-01 17:28:38 +0300

    task02done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const grandPlayerInstance = new Player(document.querySelector('#vimeo-player'));
+
+grandPlayerInstance.on(
+  'timeupdate',
+  throttle(onTimeUpdateSaveCurrentTime, 1000)
+);
+
+grandPlayerInstance.setCurrentTime(
+  localStorage.getItem('videoplayer-current-time')
+);
+
+function onTimeUpdateSaveCurrentTime(event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}
