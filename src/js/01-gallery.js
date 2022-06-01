f9c9509415a72f3fdac04d34211e047c63cb6d56
Vladyslav Avidzba <wasssd1@gmail.com>
2022-06-01 16:51:33 +0300

    something-about-t1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+// Variables
+const galleryRef = document.querySelector('div.gallery');
 
-console.log(galleryItems);
+//Calls
+renderGallery(galleryItems);
+
+//Grand magic
+const lightBoxInstance = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+//Functions
+function renderGallery(galleryItems) {
+  galleryRef.insertAdjacentHTML(
+    'beforeend',
+    galleryItems
+      .map(
+        ({ preview, original, description }) => /*html*/ `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+      )
+      .join('')
+  );
+}
